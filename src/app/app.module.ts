823a355de3447af6e6fe2e94a419f4f40f804031
Vaibhav <vaibhavkhatod30@gmail.com>
2019-03-28 19:42:05 +0530

    Native Script Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
+import { HttpClientModule } from '@angular/common/http';
+import { NativeScriptHttpModule } from "nativescript-angular/http";
+import { MenuComponent } from './menu/menu.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { DishService } from './services/dish.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
+import { baseURL } from './shared/baseurl';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -18,14 +22,20 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent
+
     ],
-    providers: [],
+    providers: [
+        {provide: 'baseURL', useValue: baseURL},
+        DishService,
+        ProcessHTTPMsgService
+    ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
